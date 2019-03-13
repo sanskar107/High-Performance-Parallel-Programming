719f6fb1df9e1c051c05a72f112aa5e994831bea
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2019-03-13 19:47:57 +0530

    Added visualizer

--- OpenMPI/Many_Body/visualizer.py
+++ OpenMPI/Many_Body/visualizer.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
-num_steps = 2116
+num_steps = 72000
 file = open('out.txt')
 data = file.read().split('\n')
 
